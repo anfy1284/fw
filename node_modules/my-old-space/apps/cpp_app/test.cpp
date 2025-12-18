3d4b00f001c099b905ff8a687b3e0c3db41ea336
Malewisch A. <anfy1284@gmail.com>
2025-12-18 17:56:48 +0100

    Cleanup: remove node_modules from this repository index

--- node_modules/my-old-space/apps/cpp_app/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello from TestApp! This is a different WASM file." << std::endl;
-    std::cout << "Running in the same shell." << std::endl;
-    return 0;
-}
--- node_modules/my-old-space/drive_forms/init.js
+++
@@ -1,2 +0,0 @@
-// Application level initialization. Place code here that should run during system startup at the application level.
-// console.log('drive_forms/init.js executed');
